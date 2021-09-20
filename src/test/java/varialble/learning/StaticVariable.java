d6cb0d0206b0e9270c54ef08ed236d221e03e3bf
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-20 16:56:06 +0530

    Static Variable

---
+++ src/test/java/varialble/learning/StaticVariable.java
@@ -0,0 +1,14 @@
+package varialble.learning;
+
+public class StaticVariable {
+    static int number = 100;
+    static char name = 'R';
+    static String name1 = "Rahul";
+    public static void main(String[] args) {
+        StaticVariable rk = new StaticVariable();
+        System.out.println(rk.number);
+        System.out.println(rk.name);
+        System.out.println(rk.name1);
+
+    }
+}
