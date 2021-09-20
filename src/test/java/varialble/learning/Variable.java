42c044fedfe12301c9aca3ea5b34664426e8df07
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-20 13:35:22 +0530

    Initial commit

---
+++ src/test/java/varialble/learning/Variable.java
@@ -0,0 +1,30 @@
+package varialble.learning;
+
+import java.awt.desktop.SystemSleepEvent;
+import java.sql.SQLOutput;
+
+public class Variable {
+    public static void main(String[] args) {
+
+
+        String name = "rahul";
+        String name2 = "himanshu";
+        String name3 = "khushi";
+        char word = 'R';
+        char word2 = 'H';
+        char word3 = 'K';
+        int number = 8;
+        int number2 = 4;
+        int number3 = 4;
+        System.out.println(name);
+        System.out.println(name2);
+        System.out.println(name3);
+        System.out.println(word);
+        System.out.println(word2);
+        System.out.println(word3);
+        System.out.println(number);
+        System.out.println(number2);
+        System.out.println(number3);
+
+    }
+}
