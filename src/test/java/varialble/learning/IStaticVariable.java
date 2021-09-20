784acc0a5303d81103e9396e164808dd8e384a11
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-20 17:45:52 +0530

    Instance Variable

---
+++ src/test/java/varialble/learning/IStaticVariable.java
@@ -0,0 +1,16 @@
+package varialble.learning;
+
+public class IStaticVariable {
+    static int number = 10;
+    static String name = "Rahul";
+    static char name1 = 'R';
+    static int number2= 100;
+
+    public static void main(String[] args) {
+        System.out.println(number);
+        System.out.println(name);
+        System.out.println(name1);
+        System.out.println(number2);
+    }
+
+}
