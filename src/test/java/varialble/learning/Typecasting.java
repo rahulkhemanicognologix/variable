6f96dcff432d9623715e6c0b8ca7120b3ec2edf8
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-21 12:37:39 +0530

    Winding

---
+++ src/test/java/varialble/learning/Typecasting.java
@@ -0,0 +1,14 @@
+package varialble.learning;
+
+public class Typecasting {
+    public static void main(String[] args) {
+        int number=10;
+        byte number2=(byte) number;
+
+        System.out.println(number2);
+
+        double number3= 200.02;
+        int number4=(int)number3;
+        System.out.println(number4);
+    }
+}
