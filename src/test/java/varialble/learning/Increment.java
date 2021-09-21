6f398de8299afa63bc6d0d35fb217c20c2958de1
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-21 13:10:30 +0530

    Increment done 2

---
+++ src/test/java/varialble/learning/Increment.java
@@ -0,0 +1,30 @@
+package varialble.learning;
+
+public class Increment {
+    public static void main(String[] args) {
+        int number = 10;
+        System.out.println(++number);
+
+        int number2=10;
+        System.out.println(number2++);
+
+        int number3= 20;
+        System.out.println(--number3);
+
+        int number4=40;
+        System.out.println(number4--);
+
+        int a = 10;
+        int b = -10;
+        String c = "True";
+        String d ="False";
+
+        System.out.println(~a);
+        System.out.println(~b);
+        System.out.println(c);
+        System.out.println(d);
+
+
+
+    }
+}
