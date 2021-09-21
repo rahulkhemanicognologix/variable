cc9877a3a209a9ba8314a13f8e31f060e804bedf
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-21 12:34:51 +0530

    Winding

---
+++ src/test/java/varialble/learning/Winding.java
@@ -0,0 +1,17 @@
+package varialble.learning;
+
+public class Winding {
+    public static void main(String[] args) {
+
+        byte number = 5;
+        int number2=number;
+        System.out.println( number2 );
+
+        float number3 = 100;
+        double number4=(number3);
+        System.out.println(number4);
+
+
+
+    }
+}
