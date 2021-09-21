2c7048f482f28170f82173a16db9df0b09de1b10
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-21 17:58:10 +0530

    Assignment Operator

---
+++ src/test/java/varialble/learning/AssignmentOperator.java
@@ -0,0 +1,20 @@
+package varialble.learning;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int a  = 10;
+        int b = 5;
+        int c = 20;
+        int d = 12;
+        a+=4;
+        b+=2;
+        a-=2;
+        c*=2;
+        d/=2;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
