29cfa94a88745b444f1a600d0f339ec62434b608
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-21 16:56:44 +0530

    OR Logical || Operator

---
+++ src/test/java/varialble/learning/LogicalOperators.java
@@ -0,0 +1,16 @@
+package varialble.learning;
+
+public class LogicalOperators {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        int c = 10;
+        int d = 112;
+        System.out.println( a>b | b>++a);
+        System.out.println(a);
+
+        System.out.println(d>c||++c>d);
+        System.out.println(c);
+
+    }
+}
