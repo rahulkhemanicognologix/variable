6dc76c00c1a6fc981deef7f6d9310f4a5769f621
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-21 13:21:29 +0530

    operator

---
+++ src/test/java/varialble/learning/AdditionSub.java
@@ -0,0 +1,23 @@
+package varialble.learning;
+
+public class AdditionSub {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int c = 8;
+
+        System.out.println(a+b);
+        System.out.println(b+c);
+        System.out.println(c+a);
+        System.out.println(c-a);
+        System.out.println(b-a);
+        System.out.println(b-c);
+        System.out.println(a*c);
+        System.out.println(c*b);
+        System.out.println(a*b);
+        System.out.println(b*c);
+        System.out.println(c/b);
+        System.out.println(b%a);
+
+    }
+}
