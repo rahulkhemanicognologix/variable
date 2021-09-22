427a89a2f442f546c7685788c02aeb2ac2775f01
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-22 13:17:51 +0530

    If else

---
+++ src/test/java/varialble/learning/IfElse.java
@@ -0,0 +1,24 @@
+package varialble.learning;
+
+import javax.swing.*;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int age = 17;
+        if ( age > 18) {
+            System.out.println("You are eligible"); }
+            else {
+            System.out.println("You are not eligible"); }
+
+            float year = 2020;
+            if (year <= 2021 ){
+                System.out.println("You are going through covid 19 ERA");}
+                else {
+                    System.out.println("You are not in covid 19 ERA");}
+                }
+
+            }
+
+
+
+
