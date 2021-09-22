aa96c6b5dd52ef3844f6764aacad6e07881e79f3
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-22 20:30:52 +0530

    Leap Year

--- src/test/java/varialble/learning/Year.java
+++ src/test/java/varialble/learning/Year.java
@@ -2,6 +2,19 @@ package varialble.learning;
 
 public class Year {
     public static void main(String[] args) {
-
+        int year = 2000;
+        if (year % 400 == 0) {
+            System.out.println(year + " Year is a leap year");
+        } else if (year % 4 == 0 && year % 100 != 0) {
+            System.out.println(year + "year is a leap year");
+        } else {
+            System.out.println(year + "Year is not a leap year");
+        }
     }
 }
+
+
+
+
+
+
