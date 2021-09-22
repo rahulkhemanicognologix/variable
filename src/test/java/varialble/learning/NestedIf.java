a2b4723888a53ce8150c8bd0fa2f31b84f3d4839
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-22 15:12:46 +0530

    Nested If else

---
+++ src/test/java/varialble/learning/NestedIf.java
@@ -0,0 +1,17 @@
+package varialble.learning;
+
+public class NestedIf {
+    public static void main(String[] args) {
+        int age = 17;
+        int weight = 57;
+        if (age > 19) {
+            if (weight >= 58) {
+                System.out.println("You are healthy");
+            } else {
+                System.out.println("You need healthy diet plan");
+            }
+        } else {
+            System.out.println("You are under age for treatment");
+        }
+    }
+}
