824ec28f23b879e4b212a7263c3796abbfdcc2b5
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-22 18:55:15 +0530

    For loop

---
+++ src/test/java/varialble/learning/ForLoop.java
@@ -0,0 +1,14 @@
+package varialble.learning;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int a = 1; a <= 10; a++) {
+            System.out.println(a);
+        }
+        {
+            for (int name = 5; name < 10; name++) {
+                System.out.println("Rahul");
+            }
+        }
+    }
+}
