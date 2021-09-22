0131baf9e8dcd73d3aefc8626c71bcb63d930b46
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-22 12:51:39 +0530

    Relational Operatior = 1 Doubt (== when created a new object then a new string are not True why ??)

---
+++ src/test/java/RelationalOperator.java
@@ -0,0 +1,32 @@
+import java.sql.SQLOutput;
+
+public class RelationalOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int c = 5;
+        int d = 6;
+        int e = 11;
+        String name = "Rahul";
+        String name2= "Rahul";
+
+        String name3 = new String ("Rahul");
+        String name4 = new String("Rahul");
+        String name5="Rahul";
+
+
+        System.out.println(a>b);
+        System.out.println(b<c);
+        System.out.println(a<=e);
+        System.out.println(d>=c);
+        System.out.println(a!=e);
+        System.out.println(c>=b);
+
+        System.out.println(name==name2);
+        System.out.println(name==name3);
+        System.out.println(name3==name4);
+        System.out.println(name4==name5);
+
+
+    }
+}
