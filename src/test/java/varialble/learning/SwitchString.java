ea695545ac5ea315e75bc60ec1c97069a3df3067
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-22 15:59:13 +0530

    Java String Switch Statement

---
+++ src/test/java/varialble/learning/SwitchString.java
@@ -0,0 +1,28 @@
+package varialble.learning;
+
+public class SwitchString {
+    public static void main(String[] args) {
+        String difficulty = "Expert";
+        int level = 0;
+
+        switch (difficulty) {
+            case "Begineer":
+                level = 1;
+                System.out.println("This is Begineer");
+                break;
+            case "High":
+                level = 2;
+                break;
+            case "Expert":
+                level = 3;
+                break;
+            default:
+                System.out.println("Begin the game to reach any level");
+        }
+        {
+            System.out.println("Your level is" );
+            System.out.println(level);
+
+        }
+    }
+}
