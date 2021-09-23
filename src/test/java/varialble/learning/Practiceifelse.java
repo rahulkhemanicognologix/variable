579e7417844a7ff4e4bee2a04d8743a8d78d10dd
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-23 11:30:13 +0530

    Practice rectangle square

---
+++ src/test/java/varialble/learning/Practiceifelse.java
@@ -0,0 +1,21 @@
+package varialble.learning;
+
+public class Practiceifelse {
+    public static void main(String[] args) {
+
+
+    int length = 10;
+    int breadth = 5;
+
+    if(( length > breadth ) && breadth < length) {
+        System.out.println("This is a rectangle");
+    }    else if (length==breadth && breadth==length) {
+        System.out.println("This is a square");
+    }
+
+         else
+            System.out.println("This is not a rectangle");
+
+        }
+}
+
