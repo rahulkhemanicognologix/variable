f6c4c74d35deba35cc63b148cbdfa5216d4f6e17
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-23 11:49:09 +0530

    Practice rectangle square

--- src/test/java/varialble/learning/Practiceifelse.java
+++ src/test/java/varialble/learning/Practiceifelse.java
@@ -4,18 +4,29 @@ public class Practiceifelse {
     public static void main(String[] args) {
 
 
-    int length = 10;
-    int breadth = 5;
+       int length = 10;
+        int breadth = 10;
 
-    if(( length > breadth ) && breadth < length) {
-        System.out.println("This is a rectangle");
-    }    else if (length==breadth && breadth==length) {
-        System.out.println("This is a square");
-    }
-
-         else
+        if ((length > breadth) && breadth < length) {
+            System.out.println("This is a rectangle");
+        } else if (length == breadth && breadth == length) {
+            System.out.println("This is a square");
+        } else
             System.out.println("This is not a rectangle");
-
+        int a = 50;
+        int  b = 30;
+        if (a> b ) {
+            System.out.println("A is bigger number ");
+        } else if(b>a) {
+            System.out.println("B is bigger number");
         }
+        else {
+            System.out.println("Both are equal");
+        }
+
+
+    }
 }
+
+
 
