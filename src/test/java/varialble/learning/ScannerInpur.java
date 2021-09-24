edd104b84c52fedc67140a16844de268cc36723e
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-24 15:20:29 +0530

    Practice rectangle square

---
+++ src/test/java/varialble/learning/ScannerInpur.java
@@ -0,0 +1,16 @@
+package varialble.learning;
+
+import java.util.Scanner;
+public class ScannerInpur {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        String name = "";
+        System.out.println(" Enter Your Name");
+        name = a.nextLine();
+        System.out.println(name);
+    }
+
+
+
+    }
+
