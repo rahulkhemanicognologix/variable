5252d106430dbf2c97a53b0874be94e3d937d34d
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-24 12:26:13 +0530

    Practice rectangle square

---
+++ src/test/java/varialble/learning/LoopWhileTry.java
@@ -0,0 +1,13 @@
+package varialble.learning;
+
+public class LoopWhileTry {
+    public static void main(String[] args) {
+        int a=2;
+        int b=0;
+        while(a<=10){
+            b=b+a;
+            a++;
+        }
+        System.out.println(b);
+    }
+}
