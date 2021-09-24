cfe64141bfa3527a914d27426a7886e51d1e6d29
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-24 15:32:07 +0530

    Practice rectangle square

--- src/test/java/varialble/learning/ScannerInpur.java
+++ src/test/java/varialble/learning/ScannerInpur.java
@@ -4,13 +4,19 @@ import java.util.Scanner;
 public class ScannerInpur {
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
-        String name = "";
-        System.out.println(" Enter Your Name");
+
+        String name = "" ;
+        System.out.println("Enter Your name");
         name = a.nextLine();
         System.out.println(name);
+
+        int number = 1;
+        System.out.println("Enter your number");
+        number =a.nextInt();
+        System.out.println(number);
+    }
+
     }
 
 
-
-    }
 
