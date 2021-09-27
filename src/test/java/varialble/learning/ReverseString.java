535397fea691e87e9368c5178a75e49e574d5085
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-09-27 15:07:02 +0530

    String method

---
+++ src/test/java/varialble/learning/ReverseString.java
@@ -0,0 +1,16 @@
+package varialble.learning;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String a = "test";
+        String b = "";
+        for (int i = a.length() - 1; i > 0; i--) {
+            b = b + a.charAt(i);
+        }
+        if (a.equals(b)) {
+            System.out.println("Palendrom String");
+        } else {
+            System.out.println("Not a Palendrom String");
+        }
+    }
+}
