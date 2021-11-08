ccc9020125c500ca0996051f97e884f781c09a90
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-11-08 12:21:27 +0530

    Inheritance

--- src/test/java/varialble/learning/MethodOverloading.java
+++ src/test/java/varialble/learning/MethodOverloading.java
@@ -1,16 +1,16 @@
 package varialble.learning;
 
  class MethodOverloading {
-   void hello (){
+   void hello (String b , int a){
        System.out.println(" This is method 1");
    }
-   void hello (int a ){
+   void hello (int a , String b){
        System.out.println(" This is method 2");
    }
 
      public static void main(String[] args) {
         MethodOverloading x = new MethodOverloading();
-        x.hello(10);
+        x.hello(10, "abc");
 
      }
 
