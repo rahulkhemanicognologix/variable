48b0cad4f74e59d3956f1d68188bd332d462ddc8
rahulkhemanicognologix <90898930+rahulkhemanicognologix@users.noreply.github.com>
2021-11-09 12:01:10 +0530

    Interface implements

--- src/test/java/varialble/learning/Interface.java
+++ src/test/java/varialble/learning/Interface.java
@@ -1,14 +1,33 @@
 package varialble.learning;
-interface rahul{
+/*interface rahul{
    public abstract   void show();
 }
  class Interface implements rahul {
     public void show() {
-        System.out.println("Hello World");
+        System.out.println("One Love");
     }
     public static void main(String[] args) {
         Interface l = new Interface();
             l.show();
 
-    }
-}
+    }*/
+  interface Khushi {
+      public abstract void hello();
+  }
+  interface Jayati {
+      public abstract void world ();
+  }
+  class Bond implements Khushi  , Jayati {
+      public void hello (){
+          System.out.println("Hello World");
+      }
+      public void world () {
+          System.out.println("Hello World Bond");
+      }
+
+      public static void main(String[] args) {
+          Bond b = new Bond();
+          b.hello();
+          b.world();
+      }
+  }
